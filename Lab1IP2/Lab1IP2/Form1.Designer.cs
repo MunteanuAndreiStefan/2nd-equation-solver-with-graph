fc7d2ee5e618724cc4ea7b6e64664589ecdfd3fe
Munteanu Andrei-Stefan <stefan@housatonic.microsoftonline.com>
2018-03-07 10:46:42 +0200

    Solution for problem 2.

---
+++ Lab1IP2/Lab1IP2/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Lab1IP2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureGraph = new System.Windows.Forms.PictureBox();
+            this.textBoxValueA = new System.Windows.Forms.TextBox();
+            this.textBoxValueB = new System.Windows.Forms.TextBox();
+            this.textBoxValueC = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.labelValueA = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelValueB = new System.Windows.Forms.Label();
+            this.labelValueC = new System.Windows.Forms.Label();
+            this.saveImageButton = new System.Windows.Forms.Button();
+            this.saveTextButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureGraph
+            // 
+            this.pictureGraph.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureGraph.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureGraph.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureGraph.Location = new System.Drawing.Point(9, 12);
+            this.pictureGraph.Name = "pictureGraph";
+            this.pictureGraph.Size = new System.Drawing.Size(180, 180);
+            this.pictureGraph.TabIndex = 1;
+            this.pictureGraph.TabStop = false;
+            // 
+            // textBoxValueA
+            // 
+            this.textBoxValueA.Location = new System.Drawing.Point(237, 51);
+            this.textBoxValueA.Name = "textBoxValueA";
+            this.textBoxValueA.Size = new System.Drawing.Size(97, 20);
+            this.textBoxValueA.TabIndex = 2;
+            this.textBoxValueA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.BoxValidateInput);
+            // 
+            // textBoxValueB
+            // 
+            this.textBoxValueB.Location = new System.Drawing.Point(237, 77);
+            this.textBoxValueB.Name = "textBoxValueB";
+            this.textBoxValueB.Size = new System.Drawing.Size(97, 20);
+            this.textBoxValueB.TabIndex = 3;
+            this.textBoxValueB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.BoxValidateInput);
+            // 
+            // textBoxValueC
+            // 
+            this.textBoxValueC.Location = new System.Drawing.Point(237, 103);
+            this.textBoxValueC.Name = "textBoxValueC";
+            this.textBoxValueC.Size = new System.Drawing.Size(97, 20);
+            this.textBoxValueC.TabIndex = 4;
+            this.textBoxValueC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.BoxValidateInput);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(194, 168);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(140, 24);
+            this.calculateButton.TabIndex = 5;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButtonClicked);
+            // 
+            // labelValueA
+            // 
+            this.labelValueA.AutoSize = true;
+            this.labelValueA.Location = new System.Drawing.Point(196, 54);
+            this.labelValueA.Name = "labelValueA";
+            this.labelValueA.Size = new System.Drawing.Size(33, 13);
+            this.labelValueA.TabIndex = 6;
+            this.labelValueA.Text = "val a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(221, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "a*x^2+b*x+c";
+            // 
+            // labelValueB
+            // 
+            this.labelValueB.AutoSize = true;
+            this.labelValueB.Location = new System.Drawing.Point(196, 80);
+            this.labelValueB.Name = "labelValueB";
+            this.labelValueB.Size = new System.Drawing.Size(36, 13);
+            this.labelValueB.TabIndex = 8;
+            this.labelValueB.Text = "val b: ";
+            // 
+            // labelValueC
+            // 
+            this.labelValueC.AutoSize = true;
+            this.labelValueC.Location = new System.Drawing.Point(196, 106);
+            this.labelValueC.Name = "labelValueC";
+            this.labelValueC.Size = new System.Drawing.Size(36, 13);
+            this.labelValueC.TabIndex = 9;
+            this.labelValueC.Text = "val c: ";
+            // 
+            // saveImageButton
+            // 
+            this.saveImageButton.Location = new System.Drawing.Point(194, 138);
+            this.saveImageButton.Name = "saveImageButton";
+            this.saveImageButton.Size = new System.Drawing.Size(63, 24);
+            this.saveImageButton.TabIndex = 10;
+            this.saveImageButton.Text = "Save Img";
+            this.saveImageButton.UseVisualStyleBackColor = true;
+            this.saveImageButton.Visible = false;
+            this.saveImageButton.Click += new System.EventHandler(this.ImageSaveGraph);
+            // 
+            // saveTextButton
+            // 
+            this.saveTextButton.Location = new System.Drawing.Point(271, 138);
+            this.saveTextButton.Name = "saveTextButton";
+            this.saveTextButton.Size = new System.Drawing.Size(63, 24);
+            this.saveTextButton.TabIndex = 11;
+            this.saveTextButton.Text = "Save txt";
+            this.saveTextButton.UseVisualStyleBackColor = true;
+            this.saveTextButton.Visible = false;
+            this.saveTextButton.Click += new System.EventHandler(this.TextSaveGraph);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 203);
+            this.Controls.Add(this.saveTextButton);
+            this.Controls.Add(this.saveImageButton);
+            this.Controls.Add(this.labelValueC);
+            this.Controls.Add(this.labelValueB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelValueA);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.textBoxValueC);
+            this.Controls.Add(this.textBoxValueB);
+            this.Controls.Add(this.textBoxValueA);
+            this.Controls.Add(this.pictureGraph);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Lab1 prob 2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureGraph;
+        private System.Windows.Forms.TextBox textBoxValueA;
+        private System.Windows.Forms.TextBox textBoxValueB;
+        private System.Windows.Forms.TextBox textBoxValueC;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label labelValueA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelValueB;
+        private System.Windows.Forms.Label labelValueC;
+        private System.Windows.Forms.Button saveImageButton;
+        private System.Windows.Forms.Button saveTextButton;
+    }
+}
+
---
+++ Lab1IP2/Lab1IP2/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+
+namespace Lab1IP2
+{
+    public partial class Form1 : Form
+    {
+        #region Values for formula constants, limits for x and the step for the graph
+
+        /// <summary>
+        /// The value of a from formula
+        /// </summary>
+        private float a = 0;
+        /// <summary>
+        /// The value of b from formula
+        /// </summary>
+        private float b = 0;
+        /// <summary>
+        /// The value of c from formula
+        /// </summary>
+        private float c = 0;
+
+        /// <summary>
+        /// The step value for the graph
+        /// </summary>
+        private float counter = 1;
+
+        /// <summary>
+        /// Minimum value of x
+        /// </summary>
+        private float xMin = -5;
+        /// <summary>
+        /// Maximum value of x
+        /// </summary>
+        private float xMax = 5;
+        /// <summary>
+        /// Minimum value of y
+        /// </summary>
+        private float yMin = -5;
+        /// <summary>
+        /// Maximum value of y
+        /// </summary>
+        private float yMax = 5;
+
+        #endregion
+
+        #region Validation functions for textBoxes and rules file.
+        /// <summary>
+        /// This function validates that the textBox contains only numbers
+        /// And numbers with decimals (ex: 0.52341)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BoxValidateInput(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar) || e.KeyChar == '.'))
+                e.Handled = e.KeyChar != (char)Keys.Back;
+        }
+
+
+        /// <summary>
+        /// Function that checks that rules.txt file exists / has valid data into it.
+        /// </summary>
+        private void ValidateRules()
+        {
+            string line;
+            try
+            {
+                StreamReader file = new StreamReader("rules.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    var nameValue = line.Split('=');
+                    if ("counter".Equals(nameValue[0]))
+                        counter = float.Parse(nameValue[1]);
+                    else if ("xMin".Equals(nameValue[0]))
+                        xMin = float.Parse(nameValue[1]);
+                    else if ("xMax".Equals(nameValue[0]))
+                        xMax = float.Parse(nameValue[1]);
+                    else if ("yMin".Equals(nameValue[0]))
+                        yMin = float.Parse(nameValue[1]);
+                    else if ("yMax".Equals(nameValue[0]))
+                        yMax = float.Parse(nameValue[1]);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Invalid format / file for rules.txt please input correct step, min, max!");
+            }
+        }
+
+
+
+        #endregion
+
+        #region Class constructor
+
+        /// <summary>
+        /// Constructor for the Form1 class.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Buttons click actions: Calculate / Save Img / Save txt
+
+        /// <summary>
+        /// This function is called when "Re/Calculate" button is pressed.
+        /// It will process the data inputed into textBoxes and it will create the graph
+        /// after that it will make buttons for save visible.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculateButtonClicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBoxValueA.Text) || String.IsNullOrEmpty(textBoxValueB.Text) || String.IsNullOrEmpty(textBoxValueC.Text))
+            {
+                MessageBox.Show("Please input values for A B C");
+            }
+            else
+            {
+                a = float.Parse(textBoxValueA.Text);
+                b = float.Parse(textBoxValueB.Text);
+                c = float.Parse(textBoxValueC.Text);
+                MakeGraph();
+                MakeSaveButtonsVisible();
+
+            }
+        }
+
+        /// <summary>
+        /// This function makes saveImageButton & saveTextButton visible and change text from
+        /// calcultateButton to "Recalculate"
+        /// </summary>
+        private void MakeSaveButtonsVisible()
+        {
+            saveImageButton.Visible = true;
+            saveTextButton.Visible = true;
+            calculateButton.Text = "Recalculate";
+        }
+
+        /// <summary>
+        /// A function that is called when pressing "Save txt" button.
+        /// It starts an new windows object of type SaveFileDialog.
+        /// It saves a list of Points in format: "Point: (x, F(x)) \n"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextSaveGraph(object sender, EventArgs e)
+        {
+            System.Text.UnicodeEncoding uniEncoding = new System.Text.UnicodeEncoding();
+            Stream streamToSaveText = new MemoryStream();
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveDialog.FilterIndex = 1;
+            saveDialog.RestoreDirectory = true;
+
+            String points = "";
+            for (float x = xMin; x <= xMax; x = x + counter)
+            {
+                points += "Point: (" + Math.Round(x, 2) + ", " + Math.Round(F(x), 2) + ")\n";
+            }
+
+            byte[] messageBytes = uniEncoding.GetBytes(points);
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((streamToSaveText = saveDialog.OpenFile()) != null)
+                {
+                    streamToSaveText.Write(messageBytes, 0, messageBytes.Length);
+                    streamToSaveText.Position = 0;
+                    streamToSaveText.Close();
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// A function that is called when button "Save img" is pressed.
+        /// It starts an new windows object of type SaveFileDialog.
+        /// It saves the graph.Image as an png.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ImageSaveGraph(object sender, EventArgs e)
+        {
+            Stream streamToSaveImage = new MemoryStream();
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "png files (*.png)|*.png|All files (*.*)|*.*";
+            saveDialog.FilterIndex = 1;
+            saveDialog.RestoreDirectory = true;
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((streamToSaveImage = saveDialog.OpenFile()) != null)
+                {
+                    pictureGraph.Image.Save(streamToSaveImage, System.Drawing.Imaging.ImageFormat.Png);
+                    streamToSaveImage.Position = 0;
+                    streamToSaveImage.Close();
+                }
+            }
+        }
+        #endregion
+
+        #region Drawning functions and F(X) calculation
+        /// <summary>
+        /// This function is the Graph draw logic
+        /// </summary>
+        private void MakeGraph()
+        {
+
+            ValidateRules();
+
+            // Make the Bitmap.
+            int width = pictureGraph.ClientSize.Width;
+            int hight = pictureGraph.ClientSize.Height;
+            Bitmap bitMap = new Bitmap(width, hight);
+            using (Graphics graph = Graphics.FromImage(bitMap))
+            {
+                graph.SmoothingMode = SmoothingMode.AntiAlias;
+                graph.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graph.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                graph.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                //graph.ScaleTransform(1, -1);
+
+                // Transform to map the graph bounds to the Bitmap.
+                RectangleF rectangle = new RectangleF(xMin, yMin, xMax - xMin, yMax - yMin);
+                PointF[] fPoints =
+                {
+                    new PointF(0, hight),
+                    new PointF(width, hight),
+                    new PointF(0, 0),
+                };
+
+                graph.Transform = new Matrix(rectangle, fPoints);
+
+                // Draw the graph.
+                using (Pen graphPen = new Pen(Color.Blue, 0))
+                {
+                    // Draw the axes.
+                    graph.DrawLine(graphPen, xMin, 0, xMax, 0);
+                    graph.DrawLine(graphPen, 0, yMin, 0, yMax);
+                    graphPen.Color = Color.Green;
+
+
+                    bool lastNumberColor = true;
+                    for (float x = xMin; x <= xMax; x = x + counter)
+                    {
+                        graph.DrawLine(graphPen, x, -0.05f, x, 0.05f);
+                        if (lastNumberColor)
+                            graph.DrawString(Math.Round(x, 2).ToString(), new Font("Arial", counter / 3, FontStyle.Regular), Brushes.Black, new PointF(x, yMax - counter / 2));
+                        else
+                            graph.DrawString(Math.Round(x, 2).ToString(), new Font("Arial", counter / 3, FontStyle.Regular), Brushes.Red, new PointF(x, yMax - counter / 2));
+                        lastNumberColor = !lastNumberColor;
+                    }
+                    lastNumberColor = true;
+                    for (float y = yMin; y <= yMax; y = y + counter)
+                    {
+                        graph.DrawLine(graphPen, -0.05f, y, 0.05f, y);
+                        if (lastNumberColor)
+                            graph.DrawString(Math.Round(y, 2).ToString(), new Font("Arial", counter / 3, FontStyle.Regular), Brushes.Black, new PointF(xMin, y));
+                        else
+                            graph.DrawString(Math.Round(y, 2).ToString(), new Font("Arial", counter / 3, FontStyle.Regular), Brushes.Red, new PointF(xMin, y));
+                        lastNumberColor = !lastNumberColor;
+                    }
+                    bitMap.RotateFlip(RotateFlipType.Rotate180FlipX);
+                    //bitMap.RotateFlip(RotateFlipType.RotateNoneFlipNone);
+
+                    graphPen.Color = Color.Red;
+
+                    // See how big 1 pixel is horizontally.
+                    Matrix inverse = graph.Transform;
+                    inverse.Invert();
+                    PointF[] PixelPoints =
+                    {
+                        new PointF(0, 0),
+                        new PointF(1, 0)
+                    };
+                    inverse.TransformPoints(PixelPoints);
+                    float dx = PixelPoints[1].X - PixelPoints[0].X;
+                    dx /= 2;
+
+                    // Loop over x values to generate points.
+                    List<PointF> points = new List<PointF>();
+                    for (float x = xMin; x <= xMax; x += dx)
+                    {
+                        bool validPoint = false;
+                        try
+                        {
+                            // Get the next point.
+                            float y = F(x);
+
+                            // If the slope is reasonable, this is a valid point.
+                            if (points.Count == 0)
+                                validPoint = true;
+                            else
+                            {
+                                float dy = y - points[points.Count - 1].Y;
+                                if (Math.Abs(dy / dx) < 1000)
+                                    validPoint = true;
+                            }
+                            if (validPoint) points.Add(new PointF(x, y));
+                        }
+                        catch
+                        {
+                        }
+
+                        // If the new point is invalid, draw
+                        // the points in the latest batch.
+                        if (!validPoint)
+                        {
+                            if (points.Count > 1)
+                                graph.DrawLines(graphPen, points.ToArray());
+                            points.Clear();
+                        }
+
+                    }
+
+                    // Draw the last batch of points.
+
+                    if (points.Count > 1)
+                        graph.DrawLines(graphPen, points.ToArray());
+                }
+            }
+
+
+            // Display the result
+            
+            pictureGraph.Image = bitMap;
+
+        }
+
+        /// <summary>
+        /// This is the core function it calculates the F(x) 
+        /// </summary>
+        /// <param name="x">The param for the F(x) function</param>
+        /// <returns></returns>
+        private float F(float x)
+        {
+            return (float)(a * Math.Pow(x, 2) + x * b + c);
+        }
+
+        #endregion
+    }
+}
